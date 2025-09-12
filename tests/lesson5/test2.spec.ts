2aa9f25e5815879d79bafaa5eefb048c26858c27
Alex <Alex@playwrightvn.com>
2025-09-12 10:50:05 +0700

    feat: add

---
+++ tests/lesson5/test2.spec.ts
@@ -0,0 +1,16 @@
+import {test} from '@playwright/test';
+
+test("Lesson05", async ({ page }) => {
+    await test.step("Navigate", async() => {
+    await page.goto("https://material.playwrightvn.com/");
+    })
+
+    await test.step("Open bài học 1", async() => {
+    await page.locator("//a[text() ='Bài học 2: Product page']").click();
+    await page.locator("//button[@class = 'add-to-cart' and @data-product-id = 1]").dblclick();
+    await page.locator("//button[@class = 'add-to-cart' and @data-product-id = 2]").click({ clickCount: 3});
+    await page.locator("//button[@class = 'add-to-cart' and @data-product-id = 3]").click();
+    })
+
+
+});
