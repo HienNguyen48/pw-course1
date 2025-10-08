ce3024fc41ba55adfe89c8649d4a0add1ea07a11
Alex <Alex@playwrightvn.com>
2025-10-08 17:31:32 +0700

    feat:LamBT10

--- pom/02.pom.product.page.ts
+++ pom/02.pom.product.page.ts
@@ -1,12 +1,13 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { MaterialBasePage } from './01.pom.materialbase.page';
 
 
 export class ProductPage02 extends MaterialBasePage {
-
+    table: Locator;
 
     constructor (page: Page){
         super(page);
+        this.table = page.locator("//table//ancestor::div[@class= 'cart']");
     }
 
     
@@ -22,12 +23,35 @@ export class ProductPage02 extends MaterialBasePage {
         await this.page.locator(this.BtnAddToCart(productName)).click({ clickCount: SL });
     }
 
-    async getXpathShoppingCartInTable(){
-        const sttRow = await this.page.locator("//tbody/tr").count();
-        const actualProductName =  await this.page.locator(`//tbody/tr[${sttRow}]//td[1]`).textContent();
-        const actualQuatity =  await this.page.locator(`//tbody/tr[${sttRow}]//td[3]`).textContent();
+    //Kiểm tra bảng hiển thị
+    async VisibleInTable(){
+        await expect(this.table).toBeVisible();
     }
-    
 
-    }
+    //Lấy toàn bộ quatity 
+     async getAllQuatity() {
+        await this.page.waitForSelector("//tbody//tr");
+        const rows = this.page.locator("//tbody//tr");
+        const rowCount = await rows.count();
+        console.log("Số hàng trong bảng:", rowCount);
+        
+        const quantities: string[] = [];
+        
+        for (let i = 1; i <= rowCount; i++) {
+            const qty = await this.page.locator(`//tbody//tr[${i}]//td[3]`).textContent();
+            const value = qty ? qty.trim() : '';
+            quantities.push(value);
+            console.log("In ra thông tin:", value);
+  }
+   return quantities;
+}
+
+     //Lấy quatity theo tên SP
+     async getQuatityByProduct(product: string) {
+        const cell = await this.page.locator(`//tr[td[1][normalize-space()='${product}']]/td[3]`);
+        const text = await cell.textContent();
+         return text ? text.trim() : '';
+     }
+
+    // }
 
--- tests/lesson-10/exercies/test2.spec.ts
+++ tests/lesson-10/exercies/test2.spec.ts
@@ -3,6 +3,9 @@ import { ProductPage02 } from 'pom/02.pom.product.page';
 import { ProductPage } from 'pom/production.page';
 
 test("Bài học 2: Product page", async ({ page }) => {
+    const number1 = 3;
+    const number2 = 2;
+    const number3 = 1;
 
     const productPageOne = new ProductPage02(page);
 
@@ -12,23 +15,30 @@ test("Bài học 2: Product page", async ({ page }) => {
     })
 
     await test.step("Thêm SP 1: 2 sản phẩm vào giỏ hàng", async() => {
-        await productPageOne.AddProductToCart("Product 1", 2);
+        await productPageOne.AddProductToCart("Product 1", number1);
     })
 
     await test.step("Thêm SP 2: 3 sản phẩm vào giỏ hàng", async() => {
-        await productPageOne.AddProductToCart("Product 2", 3);
+        await productPageOne.AddProductToCart("Product 2", number2);
     })
 
       await test.step("Thêm SP 3: 1 sản phẩm vào giỏ hàng", async() => {
-        await productPageOne.AddProductToCart("Product 3", 1);
+        await productPageOne.AddProductToCart("Product 3", number3);
     })
 
-    // await test.step("Verify thông tin trong table", async() => {
-    //     const userInfoProduct = await productPageOne.getXpathShoppingCartInTable();
+    //Kiểm tra bảng hiển thị 
+    await test.step("verify thông tin", async() => {
+        const openTable = await productPageOne.VisibleInTable();
+        console.log(`In ra thông tin: ${openTable}`)
 
-    // })
+        const allQuatities = await productPageOne.getAllQuatity();
+         console.log(`In ra thông tin: ${allQuatities}`)
 
+        const allQuatitiesByProduct = await productPageOne.getQuatityByProduct("Product 1");
+        console.log(`In ra thông tin: ${allQuatitiesByProduct}`)
 
+       expect(allQuatitiesByProduct).toBe('3');
+    });
 
 });
 
